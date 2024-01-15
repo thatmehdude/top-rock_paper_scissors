699d4ab0f82550c6dee76d177a0e3b20f151e2b0
Chisom <anaelechisom16@gmail.com>
2024-01-15 14:48:22 +0100

    add game function to start game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,7 +5,6 @@ function getComputerChoice(){
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
 };
-console.log(getComputerChoice());
 
 function playRound(playerSelection,computerSelection){
     const normalizedPlayerSelection = playerSelection.toLowerCase();
@@ -23,6 +22,24 @@ function playRound(playerSelection,computerSelection){
         return `You lose ${computerSelection} beats ${normalizedPlayerSelection}`
     }
 }
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+    for (let round = 1; round <= 5; round ++){
+        const playerChoice = prompt("Choose rock, paper or scissors: ");
+        const computerChoice = getComputerChoice();
+        let result = playRound(playerChoice, computerChoice);
+        console.log(`Round ${round}: ${result}`);
+    }
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You win the game!");
+    } else if (playerScore < computerScore) {
+        console.log("Sorry, you lose the game.");
+    } else {
+        console.log("The game is a tie.");
+    }
+}
+
+game();
